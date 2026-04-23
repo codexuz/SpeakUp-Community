1725979428af4d3849c2b7be65f241a87efb6d3f
codexuz <examonlineedumo@gmail.com>
2026-04-23 14:34:13 +0500

    "main"

--- hooks/useInAppUpdates.ts
+++ hooks/useInAppUpdates.ts
@@ -1,40 +1,43 @@
-import { useAlert } from '@/components/CustomAlert';
-import * as ExpoInAppUpdates from 'expo-in-app-updates';
-import { useEffect } from 'react';
-import { Platform } from 'react-native';
+import * as ExpoInAppUpdates from "expo-in-app-updates";
+import { useEffect } from "react";
+import { Alert, Platform } from "react-native";
 
 export const useInAppUpdates = () => {
-  const { alert } = useAlert();
-
   useEffect(() => {
-    if (__DEV__ || Platform.OS === 'web') return;
+    if (__DEV__ || Platform.OS === "web") return;
 
     const checkForUpdates = async () => {
       try {
-        const result = await ExpoInAppUpdates.checkForUpdate();
-        console.log('In-app update check result:', result);
-        if (!result.updateAvailable) return;
+        if (Platform.OS === "android") {
+          // If you want an immediate update that will cover the app with the update overlay, set it to true.
+          // More details: https://developer.android.com/guide/playcore/in-app-updates#update-flows
+          await ExpoInAppUpdates.checkAndStartUpdate(true);
+        } else {
+          const result = await ExpoInAppUpdates.checkForUpdate();
+          console.log("In-app update check result:", result);
+          if (!result.updateAvailable) return;
 
-        alert(
-          'Update available',
-          'A new version of the app is available with many improvements and bug fixes. Would you like to update now?',
-          [
-            {
-              text: 'Update',
-              onPress: async () => {
-                try {
-                  await ExpoInAppUpdates.startUpdate();
-                } catch (err) {
-                  console.error('Failed to start update:', err);
-                }
+          Alert.alert(
+            "Update available",
+            "A new version of the app is available with many improvements and bug fixes. Would you like to update now?",
+            [
+              {
+                text: "Update",
+                isPreferred: true,
+                onPress: async () => {
+                  try {
+                    await ExpoInAppUpdates.startUpdate();
+                  } catch (err) {
+                    console.error("Failed to start update:", err);
+                  }
+                },
               },
-            },
-            { text: 'Cancel' },
-          ],
-          'info',
-        );
+              { text: "Cancel" },
+            ]
+          );
+        }
       } catch (err) {
-        console.error('Update check failed:', err);
+        console.error("Update check failed:", err);
       }
     };
 
